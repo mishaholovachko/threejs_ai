745199b0bc88b0de08b4cb719e338f33be596cf6
Misha Holovachko <mishaholovachko@gmail.com>
2023-12-20 10:49:40 +0200

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+
+export {
+    CustomButton,
+    ColorPicker,
+    FilePicker,
+    AIPicker,
+    Tab,
+};
